65019c2adf4ad0c299bd79bf5112b8802ddac758
Vincent Huche <vincent@huche.net>
2020-11-17 13:16:17 +0100

    First seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :conveyings
+  has_many :conveyings, dependent: :destroy
   has_many :bookings, through: :conveyings
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :first_name, presence: true
+  validates :email, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
