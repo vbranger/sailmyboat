fc01dd03d64e70887f6849ba49dc119ef40f9ca2
Victor Branger <victor.branger@gmail.com>
2020-11-17 11:26:01 +0100

    adding schema modif

--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,10 @@ ActiveRecord::Schema.define(version: 2020_11_16_171204) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.string "phone_number"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
