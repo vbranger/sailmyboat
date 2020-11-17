f47faee62e5079876c341394112212ef3d4d14ff
pierre riedinger <riedingerpierre@gmail.com>
2020-11-17 13:16:40 +0100

    fixed db schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,6 @@ ActiveRecord::Schema.define(version: 2020_11_17_112618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "add_to_users", force: :cascade do |t|
-    t.boolean "admin"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "bookings", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "conveying_id", null: false
@@ -53,6 +47,10 @@ ActiveRecord::Schema.define(version: 2020_11_17_112618) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.string "phone_number"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
