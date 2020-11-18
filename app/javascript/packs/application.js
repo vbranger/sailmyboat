506a663d0880f0b29a0e8f19f256741723bfcadf
Victor Branger <victor.branger@gmail.com>
2020-11-18 13:38:33 +0100

    design index avec boutons pour switch des cards vers la map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,3 +34,24 @@ document.addEventListener('turbolinks:load', () => {
   // initSelect2();
   initMapbox();
 });
+
+const button_map = document.querySelector("#btn-map");
+const button_list = document.querySelector("#btn-list");
+const cards_result = document.querySelector("#cards-result");
+const map = document.querySelector('#map');
+
+button_map.addEventListener('click', (event) => {
+  button_map.classList.toggle("hidden");
+  button_list.classList.toggle("hidden");
+  cards_result.classList.toggle("hidden");
+  map.classList.toggle("hidden");
+  initMapbox();
+});
+
+button_list.addEventListener('click', (event) => {
+  button_list.classList.toggle("hidden");
+  button_map.classList.toggle("hidden");
+  cards_result.classList.toggle("hidden");
+  map.classList.toggle("hidden");
+  initMapbox();
+});
