3fd4fa06de55bf48ba5acc69c7b548fbb0e603f4
Victor Branger <victor.branger@gmail.com>
2020-11-19 20:42:31 +0100

    on revient désormais sur la show mais soucis d'affichage du calendrier

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :conveyings do
-    resources :bookings
+    resources :bookings, only: :create
   end
+  resources :booking, only: :edit
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'dashboard', to: 'pages#dashboard'
 end
