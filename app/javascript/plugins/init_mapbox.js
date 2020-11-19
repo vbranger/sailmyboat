1352dfc02b107ec3fbcfdd2b73c0589455b918c0
Elsa Lebas <lebas.elsa@gmail.com>
2020-11-19 18:54:50 +0100

    add map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -26,4 +26,29 @@ const initMapbox = () => {
   }
 };
 
-export { initMapbox };
+const fitMapToMarkersShow = (map, marker) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  bounds.extend([ marker.lng, marker.lat ]);
+  map.fitBounds(bounds, { padding: 70, maxZoom: 5, duration: 0 });
+};
+
+const initMapboxShow = () => {
+  const mapElement = document.getElementById('map-show');
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map-show',
+      style: 'mapbox://styles/mapbox/streets-v10'
+    });
+    const marker = JSON.parse(mapElement.dataset.markers);
+
+
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      // .setPopup(popup)
+      .addTo(map);
+    fitMapToMarkersShow(map, marker);
+    }
+};
+
+export { initMapbox, initMapboxShow };
