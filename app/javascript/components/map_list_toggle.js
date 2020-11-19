b91b93b06bf7b7a7cb3d4a47bc5b00efbf423477
pierre riedinger <riedingerpierre@gmail.com>
2020-11-19 17:56:21 +0100

    calendar display on show

--- app/javascript/components/map_list_toggle.js
+++ app/javascript/components/map_list_toggle.js
@@ -6,21 +6,24 @@ const initToggle = () => {
   const cards_result = document.querySelector("#cards-result");
   const map = document.querySelector('#map');
 
-  button_map.addEventListener('click', (event) => {
-    button_map.classList.toggle("hidden");
-    button_list.classList.toggle("hidden");
-    cards_result.classList.toggle("hidden");
-    map.classList.toggle("hidden");
-    initMapbox();
-  });
-
-  button_list.addEventListener('click', (event) => {
-    button_list.classList.toggle("hidden");
-    button_map.classList.toggle("hidden");
-    cards_result.classList.toggle("hidden");
-    map.classList.toggle("hidden");
-    initMapbox();
-  });
-};
+  if (button_map) {
+    button_map.addEventListener('click', (event) => {
+      button_map.classList.toggle("hidden");
+      button_list.classList.toggle("hidden");
+      cards_result.classList.toggle("hidden");
+      map.classList.toggle("hidden");
+      initMapbox();
+      });
+  }
+  if (button_list) {
+    button_list.addEventListener('click', (event) => {
+      button_list.classList.toggle("hidden");
+      button_map.classList.toggle("hidden");
+      cards_result.classList.toggle("hidden");
+      map.classList.toggle("hidden");
+      initMapbox();
+    });
+  };
+}
 
 export { initToggle };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,7 +26,7 @@ require("channels")
 import "bootstrap";
 import { initMapbox } from '../plugins/init_mapbox';
 import { initToggle } from '../components/map_list_toggle.js';
-import "../plugins/flatpickr";
+import { initCalendar } from '../plugins/flatpickr';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
@@ -35,5 +35,5 @@ document.addEventListener('turbolinks:load', () => {
   // initSelect2();
   initMapbox();
   initToggle();
+  initCalendar();
 });
-
